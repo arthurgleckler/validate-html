04321596380c7a87ed85762b6764e98b2ef31bf8
Arthur A. Gleckler <aag@alum.mit.edu>
2020-09-13 13:02:04 -0700

    Add "dedicated" per Chris Rayner.

--- validate-html.el
+++ validate-html.el
@@ -27,8 +27,9 @@
 
 ;; This file installs the command `validate-html', which sends the current
 ;; buffer to the World Wide Web Consortium's HTML Validator service and displays
-;; the results in a buffer in Compilation mode.  Use standard Compilation
-;; commands like `next-error' to move through the errors in the source buffer.
+;; the results in a dedicated buffer in Compilation mode.  Use standard
+;; Compilation commands like `next-error' to move through the errors in the
+;; source buffer.
 
 ;;; Code:
 
